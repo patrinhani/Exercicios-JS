d7974a509915a592b7dc9bdf08001c786a20ad86
Guilherme Patrinhani <103432652+patrinhani@users.noreply.github.com>
2023-05-05 10:21:17 -0300

    Add files via upload

---
+++ apostila manzano/js/calcVolume.js
@@ -0,0 +1,4 @@
+let raio = Number (prompt ('Informe o raio do objeto'))
+let altura = Number (prompt ('Altura do objeto'))
+let volume = 3.14 * raio**2 *altura
+alert ('O Volume do objeto  é de:' + volume)
---
+++ apostila manzano/js/conversaoDeFparaC.js
@@ -0,0 +1,3 @@
+let fahrenheit = parseFloat(prompt('Digite o valor em Fahrenheit:'))
+let c = (fahrenheit - 32) * (5 / 9)
+alert(fahrenheit + 'convertidos para Graus é:' + c)
