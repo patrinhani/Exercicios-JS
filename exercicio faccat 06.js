8f5c06a2fd8349c7ac57a9414a18eb634a29c656
Guilherme Patrinhani <103432652+patrinhani@users.noreply.github.com>
2023-06-08 15:47:24 -0300

    Delete exercicio faccat 06.js

--- exercicio faccat 06.js
+++
@@ -1,4 +0,0 @@
-let base = parseInt(prompt("Qual a base do triangulo: "))
-let altura = parseInt(prompt("Qual a altura do triangulo:"))
-let resultado = base * altura / 2
-alert("A area do triangulo: " + resultado)
